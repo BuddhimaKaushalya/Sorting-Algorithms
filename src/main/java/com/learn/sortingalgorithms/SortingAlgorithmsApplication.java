adfbe52b9a7007a026aa74f5288e76ccef7034e9
BuddhimaKaushalya <buddhimakaushalya.mtr@gmail.com>
2023-07-14 09:14:21 +0530

    first commit

---
+++ src/main/java/com/learn/sortingalgorithms/SortingAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.learn.sortingalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortingAlgorithmsApplication.class, args);
+	}
+
+}
